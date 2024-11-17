94157af8601db3fd5aeb10d41bcbf670d34be8a2
shadman-hayat9992 <shadmanhayat9992@gmail.com>
2024-11-18 01:13:52 +0530

    smtp mailer

---
+++ contactUs_connection.php
@@ -0,0 +1,32 @@
+<?php
+include('smtp/PHPMailerAutoload.php');
+
+echo smtp_mailer('shadman.hayat1234@gmail.com','test','second mail send through php is successfull.');
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	// $mail->SMTPDebug = 2; 
+	$mail->Username = "shadmanhayat9992@gmail.com";
+	$mail->Password = "hcgs qlyv yeod jemp";
+	$mail->SetFrom("shadmanhayat9992@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
+?>
