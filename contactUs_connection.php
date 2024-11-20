79802aac7d7a4c98ca36d70a490c6161a3f4b79a
shadman-hayat9992 <shadmanhayat9992@gmail.com>
2024-11-20 17:24:28 +0530

    contact form connection done

--- contactUs_connection.php
+++ contactUs_connection.php
@@ -1,32 +1,44 @@
 <?php
-include('smtp/PHPMailerAutoload.php');
-
-echo smtp_mailer('shadman.hayat1234@gmail.com','test','second mail send through php is successfull.');
-function smtp_mailer($to,$subject, $msg){
-	$mail = new PHPMailer(); 
-	$mail->IsSMTP(); 
-	$mail->SMTPAuth = true; 
-	$mail->SMTPSecure = 'tls'; 
-	$mail->Host = "smtp.gmail.com";
-	$mail->Port = 587; 
-	$mail->IsHTML(true);
-	$mail->CharSet = 'UTF-8';
-	// $mail->SMTPDebug = 2; 
-	$mail->Username = "shadmanhayat9992@gmail.com";
-	$mail->Password = "";
-	$mail->SetFrom("shadmanhayat9992@gmail.com");
-	$mail->Subject = $subject;
-	$mail->Body =$msg;
-	$mail->AddAddress($to);
-	$mail->SMTPOptions=array('ssl'=>array(
-		'verify_peer'=>false,
-		'verify_peer_name'=>false,
-		'allow_self_signed'=>false
-	));
-	if(!$mail->Send()){
-		echo $mail->ErrorInfo;
-	}else{
-		return 'Sent';
+	include('smtp/PHPMailerAutoload.php');
+	
+	if(isset($_POST['send']))
+	{
+		echo smtp_mailer();
 	}
-}
+	
+	function smtp_mailer()
+	{
+		$user_name = $_POST['name'];
+		$user_mail = $_POST['email'];
+		$user_contact = $_POST['contact'];
+		$user_msg = $_POST['msg'];
+		$message = "Name: $user_name <br> Email: $user_mail <br> Contact: $user_contact <br> Message: $user_msg";
+		$mail = new PHPMailer(); 
+		$mail->IsSMTP(); 
+		$mail->SMTPAuth = true; 
+		$mail->SMTPSecure = 'tls'; 
+		$mail->Host = "smtp.gmail.com";
+		$mail->Port = 587; 
+		$mail->IsHTML(true);
+		$mail->CharSet = 'UTF-8';
+		// $mail->SMTPDebug = 2;
+		$mail->Username = "";  // Sender Email Id
+		$mail->Password = "";  // Email Id Password
+		$mail->SetFrom("");    // Sender Email Id Same
+		$mail->Subject = 'Hackathon Contact';
+		$mail->Body =$message;
+		$mail->AddAddress(''); // Reciver Email Id
+		$mail->SMTPOptions=array('ssl'=>array(
+			'verify_peer'=>false,
+			'verify_peer_name'=>false,
+			'allow_self_signed'=>false
+		));
+		if(!$mail->Send()){
+			echo $mail->ErrorInfo;
+		}else{
+?>
+			<script>alert("Message Send");</script>
+<?php
+		}
+	}
 ?>
