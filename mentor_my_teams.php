f2869ab6bace23119a8eeb2f2c77bada0cd31ca3
ABDUL RAHIM <abdulrahim74264@zohomail.in>
2024-12-08 23:59:37 +0530

    Heading Changed

--- mentor_my_teams.php
+++ mentor_my_teams.php
@@ -45,7 +45,7 @@ $result = $stmt3->get_result();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Dashboard</title>
+    <title>My Team Detail</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
