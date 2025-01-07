d8ddd7f20ae9a5a83d1bdd76ccdc18585ce7330e
shadman-hayat9992 <shadmanhayat9992@gmail.com>
2025-01-07 21:29:48 +0530

    leader round page added

--- leader_problem_statement.php
+++ leader_problem_statement.php
@@ -153,9 +153,14 @@ $teamName = mysqli_query($conn, "SELECT DISTINCT team_name FROM all_team_members
         }
 
         .nav-upper-options {
-            gap: 10px;
+            gap: 0px;
             justify-content: center;
             align-items: center;
+        }
+
+        .nav {
+            padding-top: 20px;
+            padding-left: 5px;
         }
 
         .nav-upper-options h3 {
@@ -319,14 +324,21 @@ $teamName = mysqli_query($conn, "SELECT DISTINCT team_name FROM all_team_members
 
                     <a href="leader_payment.php" style="text-decoration: none;">
                         <div class="nav-option option5" style="color: black;">
-                            <i class="bi-patch-check"></i>
+                            <i class="bi-credit-card"></i>
                             <h3> Payment</h3>
+                        </div>
+                    </a>
+
+                    <a href="leader_round.php" style="text-decoration: none;">
+                        <div class="nav-option option4" style="color: black;">
+                            <i class="bi-award"></i>
+                            <h3> Rounds</h3>
                         </div>
                     </a>
 
                     <a href="leader_result.php" style="text-decoration: none;">
                         <div class="nav-option option4" style="color: black;">
-                            <i class="bi-award"></i>
+                            <i class="bi-trophy"></i>
                             <h3> Result</h3>
                         </div>
                     </a>
--- leader_team.php
+++ leader_team.php
@@ -155,9 +155,14 @@ $psId = $teamDetailsRow['psId'];
         }
 
         .nav-upper-options {
-            gap: 10px;
+            gap: 0px;
             justify-content: center;
             align-items: center;
+        }
+
+        .nav {
+            padding-top: 20px;
+            padding-left: 5px;
         }
 
         .nav-upper-options h3 {
@@ -253,16 +258,22 @@ $psId = $teamDetailsRow['psId'];
                     <a href="leader_payment.php" style="text-decoration: none;">
 
                         <div class="nav-option option3" style="color: black;">
-                            <i class="bi-patch-check"></i>
+                            <i class="bi-credit-card"></i>
                             <h3> Payment</h3>
                         </div>
                     </a>
 
+                    <a href="leader_round.php" style="text-decoration: none;">
+                        <div class="nav-option option4" style="color: black;">
+                            <i class="bi-award"></i>
+                            <h3> Rounds</h3>
+                        </div>
+                    </a>
 
                     <a href="leader_result.php" style="text-decoration: none;">
 
                         <div class="nav-option option4" style="color: black;">
-                            <i class="bi-award"></i>
+                            <i class="bi-trophy"></i>
                             <h3> Result</h3>
                         </div>
                     </a>
