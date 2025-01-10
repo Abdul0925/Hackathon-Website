031cc8908b897bce1c22253f75c91f0558e42a3c
shadman-hayat9992 <shadmanhayat9992@gmail.com>
2025-01-10 14:54:39 +0530

    admin vab bar fixed

--- admin_add_notifications.php
+++ admin_add_notifications.php
@@ -58,21 +58,20 @@ if ($_SESSION['admin_logged_in'] != true) {
         }
 
         .nav-upper-options {
-            /* gap: 10px; */
+            gap: 0px;
             justify-content: center;
             align-items: center;
         }
 
         .nav-upper-options h3 {
-            font-size: 18px;
-            /* margin-bottom: 0px; */
+            font-size: 16px;
             font-weight: bold;
             padding-left: 10px;
             text-decoration: none;
         }
 
         .nav-option i {
-            font-size: 185%;
+            font-size: 160%;
         }
     </style>
 
